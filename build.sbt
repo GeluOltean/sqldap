bb5bbfa269f45347cf155b191da902b0454db765
Gelu Oltean <geluoltean8@gmail.com>
2020-02-16 20:08:07 +0200

    Scaffold project

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "ldap-dsl"
+
+version := "0.1"
+
+scalaVersion := "2.13.1"
+
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,11 @@
+import java.util.Properties
+
+import scala.jdk.CollectionConverters._
+import javax.naming.{Context, NamingEnumeration}
+import javax.naming.directory.{BasicAttributes, InitialDirContext, SearchControls, SearchResult}
+
+object Main {
+  def main(args: Array[String]): Unit = {
+
+  }
+}
