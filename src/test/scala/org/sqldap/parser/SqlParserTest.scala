e02f48e50eb118b4767ecd93a3c0e6efd4b23db5
Gelu Oltean <geluoltean8@gmail.com>
2020-02-21 10:33:43 +0200

    Extend SqlParser tests

--- src/test/scala/org/sqldap/parser/SqlParserTest.scala
+++ src/test/scala/org/sqldap/parser/SqlParserTest.scala
@@ -13,7 +13,28 @@ class SqlParserTest extends AnyFlatSpec with Matchers with TryValues {
   val attributes: Seq[(String, String)] = Seq(("a", "A"), ("b", "B"))
   val expected: SelectStruct = SelectStruct(fields, table, attributes)
 
-  "The select statement" should "parse correctly" in {
+  "Select" should "parse the selected fields" in {
+    val parseResult: Try[ArraySeq[String]] = new SqlParser(f"SELECT ${fields.reduce((x, y) => f"$x,$y")}")
+      .select
+      .run()
+    parseResult.success.value should equal(fields)
+  }
+
+  "From" should "parse the table name" in {
+    val parseResult: Try[String] = new SqlParser(f"FROM $table")
+      .from
+      .run()
+    parseResult.success.value should equal(table)
+  }
+
+  "Where" should "parse the attributes" in {
+    val parseResult: Try[Seq[(String, String)]] = new SqlParser(f"WHERE ${attributes.map(tup => f"${tup._1} = ${tup._2}").reduce((x, y) => f"$x AND $y")}")
+      .where
+      .run()
+    parseResult.success.value should equal(attributes)
+  }
+
+  "The select statement" should "parse correctly into SelectStruct" in {
     val parseResult: Try[SelectStruct] = new SqlParser(f"SELECT ${fields.reduce((x, y) => f"$x,$y")} FROM $table " +
       f"WHERE ${attributes.map(tup => f"${tup._1} = ${tup._2}").reduce((x, y) => f"$x AND $y")}" +
       ";")
