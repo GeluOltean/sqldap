f077581da4cf501f18660898b365f5f3ecb4caa2
Gelu Oltean <geluoltean8@gmail.com>
2020-02-21 10:21:50 +0200

    Refactor from protected to protected[package]

--- src/main/scala/org/sqldap/parser/SqlParser.scala
+++ src/main/scala/org/sqldap/parser/SqlParser.scala
@@ -8,27 +8,27 @@ case class SelectStruct(fields: ArraySeq[String], table: String, attributes: Seq
 
 //noinspection TypeAnnotation since IntelliJ IDEA goes mad from lack of implicits on return types
 class SqlParser(val input: ParserInput) extends Parser {
-  protected def space = rule { oneOrMore(atomic(" ")) }
+  protected[parser] def space = rule { oneOrMore(atomic(" ")) }
 
-  protected def maybeSpace = rule { zeroOrMore(atomic(" ")) }
+  protected[parser] def maybeSpace = rule { zeroOrMore(atomic(" ")) }
 
-  protected def chars = rule { oneOrMore(CharPredicate.AlphaNum)}
+  protected[parser] def chars = rule { oneOrMore(CharPredicate.AlphaNum)}
 
-  protected def comma = rule { maybeSpace ~ atomic(",") ~ maybeSpace }
+  protected[parser] def comma = rule { maybeSpace ~ atomic(",") ~ maybeSpace }
 
-  protected def fieldCondition = rule {
+  protected[parser] def fieldCondition = rule {
     zeroOrMore(atomic("and" | "AND" ) ~ space) ~ capture(chars) ~ maybeSpace ~ atomic("=") ~ maybeSpace ~ capture(chars) ~> { (key: String, value: String) => Tuple2(key, value) }
   }
 
-  protected def select = rule {
+  protected[parser] def select = rule {
     atomic("SELECT" | "select") ~ space ~ capture(oneOrMore(CharPredicate.AlphaNum).separatedBy(comma)) ~> { fields: String => ArraySeq.from(fields.replace(" ", "").split(",")) }
   }
 
-  protected def from = rule {
+  protected[parser] def from = rule {
     atomic("FROM" | "from") ~ space ~ capture(chars) ~> { table: String => table }
   }
 
-  protected def where = rule {
+  protected[parser] def where = rule {
     atomic("WHERE" | "where") ~ space ~ oneOrMore(fieldCondition).separatedBy(space) ~> { attributes: Seq[(String, String)] => attributes }
   }
 
